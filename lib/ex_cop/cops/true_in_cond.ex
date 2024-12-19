joeyates/elixir-style-guide-formatter

Replace final, always-matching atoms in cond with 'true'

lib/ex_cop/cops/true_in_cond.ex
@@ -1,0 +1,30 @@
+defmodule ExCop.Cops.TrueInCond do
+  @moduledoc """
+  This cop ensures final, always-matching clauses in `cond` use `true`.
+  """
+  import Access
+
+  def apply({forms, comments}) do
+    forms =
+      Macro.prewalk(forms, fn
+        {:cond, cond_context, [[{{:__block__, do_context, [:do]}, clauses}]]} ->
+          last_match = get_in(clauses, las_match_path())
+
+          clauses =
+            if is_atom(last_match) do
+              update_in(clauses, las_match_path(), fn _ -> true end)
+            else
+              clauses
+            end
+
+          {:cond, cond_context, [[{{:__block__, do_context, [:do]}, clauses}]]}
+
+        other ->
+          other
+      end)
+
+    {forms, comments}
+  end
+
+  defp las_match_path(), do: [at(-1), elem(2), at(0), at(0), elem(2), at(0)]
+end

joeyates/green

lib/ex_cop/cops/true_in_cond.ex
@@ -1,0 +1,30 @@
+defmodule ExCop.Cops.TrueInCond do
+  @moduledoc """
+  This cop ensures final, always-matching clauses in `cond` use `true`.
+  """
+  import Access
+
+  def apply({forms, comments}) do
+    forms =
+      Macro.prewalk(forms, fn
+        {:cond, cond_context, [[{{:__block__, do_context, [:do]}, clauses}]]} ->
+          last_match = get_in(clauses, last_match_path())
+
+          clauses =
+            if is_atom(last_match) do
+              update_in(clauses, last_match_path(), fn _ -> true end)
+            else
+              clauses
+            end
+
+          {:cond, cond_context, [[{{:__block__, do_context, [:do]}, clauses}]]}
+
+        other ->
+          other
+      end)
+
+    {forms, comments}
+  end
+
+  defp last_match_path(), do: [at(-1), elem(2), at(0), at(0), elem(2), at(0)]
+end