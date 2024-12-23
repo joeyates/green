0ae9f2861468daefb59a6f1f25bf036754e5220b
Joe Yates <joe.g.yates@gmail.com>
2024-12-23 21:14:59 +0100

    Get the same quoted data as we have in code

--- bin/dump-forms.exs
+++ bin/dump-forms.exs
@@ -1,7 +1,16 @@
 #!/usr/bin/env elixir
 
+to_quoted_opts =
+  [
+    unescape: false,
+    literal_encoder: &{:ok, {:__block__, &2, [&1]}},
+    token_metadata: true,
+    emit_warnings: false
+  ]
+
 System.argv()
 |> hd()
 |> File.read!()
-|> Code.string_to_quoted!()
+|> Code.string_to_quoted_with_comments!(to_quoted_opts)
+|> elem(0)
 |> IO.inspect()
