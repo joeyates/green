6818e1286b3fc395430972701eaf44c8dd8a6582
Joe Yates <joe.g.yates@gmail.com>
2025-01-21 09:25:54 +0100

    Prepare first release

--- mix.exs
+++ mix.exs
@@ -6,10 +6,29 @@ defmodule ExCop.MixProject do
       app: :ex_cop,
       version: "0.1.0",
       elixir: "~> 1.14",
-      elixirc_paths: elixirc_paths(Mix.env())
+      deps: deps(),
+      description: "Format Elixir code according to a consistent style",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package()
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/joeyates/ex_cop"
+      },
+      maintainers: ["Joe Yates"]
+    }
+  end
 end
