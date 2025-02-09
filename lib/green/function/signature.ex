9f1ba2a6ac190cd6357110796ffce085d5d7feb4
Joe Yates <joe.g.yates@gmail.com>
2025-02-09 10:17:46 +0100

    Remove redundant comment

--- lib/green/function/signature.ex
+++ lib/green/function/signature.ex
@@ -1,10 +1,6 @@
 defmodule Green.Function.Signature do
   @moduledoc """
   This module defines a struct that represents a function signature.
-
-  N.B. We store names and modules as Strings, because, otherwise we would have to
-  convert Strings to possibly non-existent Atoms when splitting the module(s) from
-  the function name.
   """
 
   @enforce_keys [:name, :arity]
